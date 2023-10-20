7370f6f42122b11ed9a54fb40375d214712c2bc4
Saber Heydari <silverethical@gmail.com>
2023-10-20 14:00:56 +0330

    Feat(04): Working with fs module
    Description:
    Use `fs` module to create file with content, append content to file, rename file, and delete file.

---
+++ 04-filesystem/main.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+// fs.writeFile("./test.txt", "Hello World!", (err) => {
+// 	if (err) throw err;
+
+// 	console.log("Data saved.");
+// });
+
+// fs.appendFile("./test.txt", "\nSaber", (err) => {
+// 	if (err) throw err;
+
+// 	console.log("Data appended.");
+// });
+
+// fs.rename("./text.txt", "sabra.txt", (err) => {
+// 	if (err) throw err;
+
+// 	console.log("file renamed.");
+// });
+
+// fs.unlink("./sabra.txt", (err) => {
+// 	if (err) throw err;
+
+// 	console.log("file deleted.");
+// });
