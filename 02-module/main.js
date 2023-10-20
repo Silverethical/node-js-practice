056673160fa999eb83d82ba33861b4d5bb7db217
Saber Heydari <silverethical@gmail.com>
2023-10-20 13:59:44 +0330

    Feat(02: os): Playing with the `os` module

--- 02-module/main.js
+++ 02-module/main.js
@@ -6,3 +6,27 @@ const { sayHi, myName } = require("./logger");
 
 // Log 'myName' in blue.
 sayHi(chalk.blue(myName));
+
+// Print a visual separator to the console for clarity.
+console.log("--------------------------");
+
+// Import the 'os' module to access OS-related information.
+const os = require("os");
+
+// Print the platform (e.g., "linux", "win32") to the console.
+console.log("platform:", os.platform());
+
+// Print the architecture (e.g., "x64") to the console.
+console.log("arch:", os.arch());
+
+// Print information about the current user to the console.
+console.log("userInfo:", os.userInfo());
+
+// Print the OS release (e.g., "10.0.19041") to the console.
+console.log("release:", os.release());
+
+// Print the hostname (e.g., computer name) to the console.
+console.log("hostname:", os.hostname());
+
+// Print the system uptime (in seconds) to the console.
+console.log("uptime:", os.uptime());
